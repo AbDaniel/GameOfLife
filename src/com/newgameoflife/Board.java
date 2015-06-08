d01c56037ece533af52197e318486778674ad3f6
daniel <abrahamdaniel92@gmail.com>
2015-06-08 13:40:56 +0530

    Refactor : Changed Board constructor to get Cells instead of length and breadth

--- src/com/newgameoflife/Board.java
+++ src/com/newgameoflife/Board.java
@@ -7,15 +7,8 @@ public class Board {
 
     List<List<Cell>> cells;
 
-    public Board(int length, int breadth) {
-        cells = new ArrayList<List<Cell>>();
-        for (int i = 0; i < length; i++) {
-            List<Cell> row = new ArrayList<Cell>();
-            for (int j = 0; j < breadth; j++) {
-                row.add(new Cell());
-            }
-            cells.add(row);
-        }
+    public Board(List<List<Cell>> cells) {
+        this.cells = cells;
     }
 
     @Override
